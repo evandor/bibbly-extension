77c02c33b4f755bf2930d6085c53b7811649d532
Carsten Gräf <evandor@gmail.com>
2024-06-28 15:48:42 +0200

    wip

---
+++ src/pages/sidepanel/helper/TabDetailsSearchIndex.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="col-12 text-caption">
+    <div v-for="(k,index) in searchIndex">
+      <div class="row" v-if="searchIndex.get(index)['v']">
+        <div class="col-4 q-ml-sm text-bold">
+          {{ searchIndex.get(index)['name'] }}
+        </div>
+        <div class="col-7 ellipsis">
+          {{ searchIndex.get(index)['v'] }}
+          <q-tooltip class="tooltip">{{ searchIndex.get(index)['v'] }}</q-tooltip>
+        </div>
+        <div class="col text-right">
+          <q-icon name="o_check_circle" color="primary"/>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script lang="ts" setup>
+
+import {onMounted, ref, watchEffect} from "vue";
+import _ from "lodash";
+import {Tab} from "src/tabsets/models/Tab";
+import {useTabsetsStore} from "src/tabsets/stores/tabsetsStore";
+
+const props = defineProps({
+  tabId: {type: String, required: true}
+})
+
+const tab = ref<Tab | undefined>(undefined)
+const searchIndex = ref<any>()
+
+onMounted(() => {
+  const tabObject = useTabsetsStore().getTabAndTabsetId(props.tabId)
+  if (tabObject) {
+    tab.value = tabObject.tab
+  }
+})
+
+watchEffect(() => {
+  console.log("tabid", props.tabId)
+
+
+
+})
+</script>
---
+++ src/stores/authStore.ts
@@ -0,0 +1,57 @@
+import {defineStore} from 'pinia';
+import PersistenceService from "src/services/PersistenceService";
+import {computed, ref} from "vue";
+import {Account} from "src/models/Account";
+
+export enum AccessItem {
+  SYNC = "SYNC",
+  SHARE = "SHARE",
+  FEATURE_TOGGLES = "FEATURE_TOGGLES"
+}
+
+/**
+ * dummy store for submodules integration
+ */
+export const useAuthStore = defineStore('auth', () => {
+
+  // let storage = null as unknown as PersistenceService
+  //
+  // const authenticated = ref(false)
+  const user = ref<any>(null)
+  // const authRequest = ref<string>(null as unknown as string)
+  // const account = ref<Account | undefined>(undefined)
+  //
+  // const products = ref<string[]>([])
+
+
+  // --- init ---
+  async function initialize(ps: PersistenceService) {
+  }
+
+  // --- getters ---
+  const isAuthenticated = computed(() => {
+    return false
+  })
+
+  const getUsername = computed(() => {
+    return "anonymous"
+  })
+
+  const getAccount = computed(() => {
+    return (): Account | undefined => undefined
+  })
+
+  // --- actions ---
+  async function setUser(u: any | undefined) {
+    console.log("noop: setting user id to ", u?.uid)
+  }
+
+  return {
+    initialize,
+    user,
+    isAuthenticated,
+    getUsername,
+    setUser,
+    getAccount,
+  }
+})
