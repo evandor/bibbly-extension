56b63ab4a26a26fc040a056f8adde1a8e925595e
Carsten Gräf <evandor@gmail.com>
2024-07-29 18:59:12 +0200

    content editing

--- src-bex/dom.ts
+++ src-bex/dom.ts
@@ -1,10 +1,36 @@
 // Hooks added here have a bridge allowing communication between the Web Page and the BEX Content Script.
 // More info: https://quasar.dev/quasar-cli/developing-browser-extensions/dom-hooks
 import {bexDom} from 'quasar/wrappers'
-import { diffChars } from "diff"
+import {diffChars} from "diff"
+import {useUtils} from "src/core/services/Utils";
+
 // import {execute} from 'htmldiff-js';
 
+const {sendMsg} = useUtils()
+
 export default bexDom((bridge) => {
+
+  // function getCEOfCaret() {
+  //   const selection = window.getSelection()
+  //   if (!selection) {
+  //     console.log("no selection")
+  //     return null
+  //   }
+  //   if (selection.rangeCount === 0) {
+  //     console.log("no selection range")
+  //     return null
+  //   }
+  //   const range = selection.getRangeAt(0)
+  //   const start = range.startContainer;
+  //   let startElement;
+  //   if (start.nodeType === 1) {
+  //     startElement = start;
+  //   } else {
+  //     startElement = start.parentElement
+  //   }
+  //   return startElement ? startElement.closest('[contenteditable="true"]') : null
+  // }
+
   document.addEventListener('focusin', function (e: FocusEvent) {
     // console.log('focusin!', e)
     // console.log('focusin!', e.target)
@@ -13,6 +39,7 @@ export default bexDom((bridge) => {
     e.target.dataset.originalHtml = e.target.innerHTML
     // @ts-ignore
     console.log("set data to", e.target.dataset.originalHtml)
+
   })
   document.addEventListener('focusout', function (e: FocusEvent) {
 
@@ -27,35 +54,44 @@ export default bexDom((bridge) => {
     console.log("diff", diff)
 
     let fragment = document.createDocumentFragment();
-    diff.forEach((part:any) => {
+    let html = ''
+    diff.forEach((part: any) => {
       // green for additions, red for deletions
       // grey for common parts
       const color = part.added ? 'green' :
         part.removed ? 'red' : 'grey';
       const span = document.createElement('span');
-      span.style.color = color;
-      console.log("part.value", part.value)
+      span.style.backgroundColor = color;
+      //console.log("part.value", part.value)
+
       var doc = new DOMParser().parseFromString(part.value, "text/html");
       // span.appendChild(doc.innerHTML);
       span.innerHTML = part.value
       console.log("span", span)
       fragment.appendChild(span);
+      html += span.outerHTML
+
     });
 
-    console.log(fragment);
+    console.log("parsing", html)
+    const domFromHtml = new DOMParser().parseFromString(html, "text/html");
 
-    let div=document.createElement("div");
-    div.appendChild(fragment);
-    console.log("===>", div.innerHTML)
+    //console.log(fragment);
 
-    target.innerHTML = div.innerHTML
+    // let div=document.createElement("div");
+    // div.appendChild(domFromHtml);
+    // console.log("===>", div.innerHTML)
+
+    target.innerHTML = domFromHtml.body.innerHTML
+
+    sendMsg('sending-message', {html: document.body.innerHTML})
 
     //target.appendChild(fragment.textContent)
-   // document.body.appendChild(fragment)
-    document.body.insertAdjacentHTML(
-      "beforeend",
-      div.innerHTML
-    );
+    // document.body.appendChild(fragment)
+    //  document.body.insertAdjacentHTML(
+    //    "beforeend",
+    //    div.innerHTML
+    //  );
 
   })
   //alert("sendingsending")
